4ed09a0a18f74e2e28bb3638683acb48f4fd2ff5
khubaibsiddiqui <khubaib.zahid@gmail.com>
2018-04-03 20:13:23 -0400

    database

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const MONGO_URL = "mongodb://localhost:27017/scratch-map"
+const MONGO_URL = "mongodb://<khubaib>:<scratchmap123>@ds133659.mlab.com:33659/scratchmap"
 const CLIENT_ID = "1592972444132000"
 const CLIENT_SECRET = "8d9dc441c8a07a335e46a76b35c89aab"
 
