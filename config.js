7bbfa954800f314bdbaee4837114799e0b60038b
khubaibsiddiqui <khubaib.zahid@gmail.com>
2018-04-03 23:33:35 -0400

    database testing 5

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const MONGO_URL = "mongodb://<khubaib>:<scratchmap123>@ds133659.mlab.com:33659/scratchmap"
+const MONGO_URL = "mongodb://<dbuser>:<dbpassword>@ds133659.mlab.com:33659/scratch-map2"
 const CLIENT_ID = "1592972444132000"
 const CLIENT_SECRET = "8d9dc441c8a07a335e46a76b35c89aab"
 
