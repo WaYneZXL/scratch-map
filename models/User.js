6674b05e7da70127843fce254aed2b61cbbe1f13
khubaibsiddiqui <khubaib.zahid@gmail.com>
2018-04-04 01:45:45 -0400

    database 4

--- models/User.js
+++ models/User.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-var UserSchema = new mongoose.Schema({
+var userSchema = new mongoose.Schema({
   name: String,
   userid: String,
   updated_at: { type: Date, default: Date.now },
 });
 
-UserSchema.statics.findOrCreate = require("find-or-create");
+userSchema.statics.findOrCreate = require("find-or-create");
 
 module.exports = mongoose.model('User', userSchema);
