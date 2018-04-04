aae68872e6a519f116a7f22488bbf93d24100f77
khubaibsiddiqui <khubaib.zahid@gmail.com>
2018-04-04 02:20:03 -0400

    address correction

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const MONGO_URL = "mongodb://<dbuser>:<dbpassword>@ds133659.mlab.com:33659/scratch-map2"
+const MONGO_URL = "mongodb://scratchmap:scratchmap@ds233769.mlab.com:33769/scratch-map-3"
 const CLIENT_ID = "1592972444132000"
 const CLIENT_SECRET = "8d9dc441c8a07a335e46a76b35c89aab"
 
